bfabf1fa2fdd7c312387dc47ff247aac4e5884f9
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-05-27 01:37:22 +0200

    Conform to Berne Convention

--- coffeebreak.sh
+++ coffeebreak.sh
@@ -6,8 +6,6 @@
 # Copyright 2013 Devin Teske
 # Copyright 2018 Mateusz Piotrowski <0mp@FreeBSD.org>
 #
-# All rights reserved.
-#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
